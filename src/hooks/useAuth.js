e48156735bf13b72dd6c08161f17e0f59176051f
fran6jr <fakabuogu@gmail.com>
2022-02-17 01:19:30 +0100

    Partially Implemented auth for routes

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 
+const authContext = createContext();
 
+const AuthProvider = ({ children }) => {
 
-function useAuth() {
-  
+  const auth = useAuth(); //initialise the auth state
 
-  return {auth: login};
+  return <AuthContext.Provider value ={auth} >
+    { children }
+  </AuthContext.Provider>
 }
 
-export default useAuth;
+const AuthConsumer = () => {// use this to access the state from the child component
+  return useContext(authContext);
+}
+
+const useAuth = () => {
+
+
+  return {
+    user,// I want user to contain the user data
+    login,
+    logout,
+    signup
+  }
+}
+
+export default AuthConsumer;
+export default AuthProvider;
